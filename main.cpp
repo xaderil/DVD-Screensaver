35e857f4a03d8360ccf7cf38171177418d3cfbee
Xaderil <hjbgbj@gmail.com>
2021-10-25 01:04:19 +1000

    First stable version with horizontal motion

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
+#define resolutionWidth 1280
+#define resolutionHeight 720
+#define framerateLimit 90
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(resolutionWidth, resolutionHeight), "My window");
+    window.setFramerateLimit(framerateLimit);
+
+    sf::Texture textureDvd;
+    if(!textureDvd.loadFromFile("src/dvd.png")) {
+        std::cout << "Could not load DVD picture" << std::endl;
+    }
+    sf::Sprite dvdPict;
+    dvdPict.setTexture(textureDvd);
+    dvdPict.scale(0.17, 0.17);
+    dvdPict.setPosition(100, 100);
+    dvdPict.setColor(sf::Color::Yellow);
+
+    int32_t moveX = 10;
     while (window.isOpen())
     {
         sf::Event event;
@@ -13,13 +28,16 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        window.clear(sf::Color::Blue);
 
-        window.clear(sf::Color::Black);
-
-        sf::CircleShape shape(100.f);
-        shape.setFillColor(sf::Color::Green);
-        window.draw(shape);
-
+        sf::Vector2f shapePosition = dvdPict.getPosition();
+        std::cout << shapePosition.x << " " << shapePosition.y << std::endl;
+        std::cout << resolutionWidth - shapePosition.x << " " << resolutionWidth - shapePosition.x + 270.5 << std::endl << std::endl;
+        if (resolutionWidth - shapePosition.x - 270.2 <= 0 || resolutionWidth - shapePosition.x >= resolutionWidth) {
+            moveX = -moveX;
+        }
+        dvdPict.move(moveX, 0);
+        window.draw(dvdPict);
         window.display();
     }
 
